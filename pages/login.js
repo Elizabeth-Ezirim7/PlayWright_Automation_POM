179d72cdddec3941e93cf808d69e89df10e16872
Elizabeth-Ezirim7 <testcad77@gmail.com>
2024-10-29 10:41:58 -0700

    first commit

--- pages/login.js
+++ pages/login.js
@@ -4,24 +4,26 @@ exports.LoginPage = class LoginPage {
 
         this.page = page
         this.harmburger_button = page.getByRole('img', { name: 'menu bar' })
-        this.first_login_button = page.getByRole('navigation')
+        this.first_login_button = page.getByRole('navigation').getByText('Log In')
         this.username_textbox = page.getByPlaceholder('Username/Email')
         this.password_textbox = page.getByPlaceholder('Password')
         this.login_button = page.getByRole('button', { name: 'Login' })
+        this.logout_button = page.getByRole('button', { name: 'input icon Logout' })
 
     }
 
-    async gotoLoginPage(){
+    async gotoLoginPage() {
         await this.page.goto('https://asp-vultelanding3-prod-southuk-01-qa.azurewebsites.net/')
     }
 
-    async login(username, password){
+    async login(username, password) {
 
         await this.harmburger_button.click()
         await this.first_login_button.click()
         await this.username_textbox.fill(username)
         await this.password_textbox.fill(password)
         await this.login_button.click()
+        await this.logout_button.click()
 
     }
 
